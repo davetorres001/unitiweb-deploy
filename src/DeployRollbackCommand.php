ae06f962e393a79da1435ace4f4ee9b052250eab
Dave Torres <dave@busybusy.com>
2018-01-17 19:10:09 -0700

    Completed the rollback command

--- src/DeployRollbackCommand.php
+++ src/DeployRollbackCommand.php
@@ -7,20 +7,31 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
 use Unitiweb\Deploy\Common\Config;
 use Unitiweb\Deploy\Common\DeployOutput;
 use Unitiweb\Deploy\Common\DeployProcess;
+use Unitiweb\Deploy\Common\Env;
 use Unitiweb\Deploy\Process\ComposerInstallProcess;
 use Unitiweb\Deploy\Process\ProcessInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 class DeployRollbackCommand extends Command
 {
     use LockableTrait;
 
+    const RELEASE_DATE_FORMAT = 'M jS, Y h:i:s a';
+
     /**
      * @var string
      */
     protected $configPath;
+
+    /**
+     * @var Env
+     */
+    protected $env;
 
     /**
      * @var Config
@@ -61,54 +72,193 @@ class DeployRollbackCommand extends Command
     {
         assert(valid_num_args());
 
+        $this->output = new DeployOutput($output, $input);
+
         // Make sure the deployment is not current running
         if (!$this->lock()) {
             $this->output->error('The command is already running in another process.');
         }
 
-        $this->output = new DeployOutput($output, $input);
-        $this->config = new Config($this->output, null, $this->configPath);
-        $this->config->load($this->output);
-        $this->process = new DeployProcess($this->output, $this->config);
+        // Load the configuration
+        $this->env = new Env($this->output, null, $this->configPath);
+        $this->env->load();
 
-        $root = $this->config->getPath('Root');
-        $releases = $this->config->getPath('Releases');
-        $sudo = true === $this->config->getUseSudo() ? 'sudo' : '';
-        $group = $this->config->getPermissionsProcess();
+        // Load the configuration
+        $this->config = new Config($this->output, $this->configPath);
+        $this->config->load();
 
-        $this->output->header('Rolling Back Release');
+        $this->process = new DeployProcess($this->output, $this->env);
 
-        $dirs = [];
-        foreach (scandir($releases) as $dir) {
-            $path = $releases . $dir;
-            if (is_dir($path) && substr($dir, 0, 1) !== '.') {
-                array_push($dirs, $path);
+        $this->releases();
+    }
+
+    /**
+     * Slow a list of current releases in revers order and allow the user to choose one
+     */
+    protected function releases()
+    {
+        assert(valid_num_args());
+
+        $release = $this->askForRelease();
+
+        if (null !== $release) {
+            $this->rollbackTo($release);
+        }
+
+        $this->output->header('Deploy Complete');
+        $this->output->line('yellow');
+        $this->output->blank();
+    }
+
+    /**
+     * Ask to select a release and return
+     */
+    protected function askForRelease() : ?string
+    {
+        assert(valid_num_args());
+
+        $current = $this->env->getCurrent();
+
+        $this->output->header('Rollback: Choose Release (listed: newest to oldest');
+        $releases = $this->listReleases();
+        $this->generateReleasesTable($releases);
+
+        $release = null;
+        $count = count($releases);
+
+        while (true) {
+            $answer = $this->output->ask('Enter the index of the release to restore:');
+            $selected = (int) $answer;
+
+            if ($selected === 0) {
+                $this->output->writeln('');
+                $this->output->writeln('Rollback Cancelling');
+                $this->output->line();
+                break;
+            }
+
+            if ($selected > $count) {
+                $this->output->writeln('');
+                $this->output->writeln('Selection is out of range');
+                $this->output->line();
+                continue;
+            }
+
+            if ($releases[$selected] === $current) {
+                $this->output->writeln('');
+                $this->output->writeln('Selected Release is already live');
+                $this->output->line();
+                break;
+            }
+
+            if ($selected > 0) {
+                if (isset($releases[$selected])) {
+                    $release = $releases[$selected];
+                    break;
+                }
             }
         }
 
-        if (count($dirs) <= 1) {
-            $this->output->error('There is no release to rollback to');
+        return $release;
+    }
+
+    /**
+     * List the current releases
+     */
+    protected function listReleases() : array
+    {
+        assert(valid_num_args());
+
+        $releases = [];
+
+        $path = $this->env->getReleasesPath();
+
+        $process = new Process("ls -r $path");
+        $process->run();
+        $lines = explode("\n", $process->getOutput());
+
+        foreach ($lines as $index => $line) {
+            $line = trim($line);
+            if ($line !== '' && substr($line, 0, 1) !== '.') {
+                $releases[$index + 1] = $line;
+            }
         }
 
-        rsort($dirs);
+        return $releases;
+    }
 
-        $current = $dirs[0];
-        $source = $dirs[1];
 
-        $this->output->writeln('Create symlink to previous release');
-        $this->process->run("$sudo rm current", $root);
-        $this->process->run("$sudo ln -s $source $root/current");
+    /**
+     * Generate tags table
+     */
+    protected function generateReleasesTable(array $releases)
+    {
+        assert(valid_num_args());
 
-        $this->output->writeln('Removing current release');
-        if ($group !== '') {
-            $this->process->run("$sudo chown -R $group var/cache var/logs var/sessions", $current);
+        $current = $this->env->getCurrent();
+
+        $table = new Table($this->output->getOutput());
+        $table->setHeaders(array('Index', 'Release Date <fg=yellow>(current in yellow)</>'));
+        $rows = [];
+        array_push($rows, [0, "Cancel Rollback"]);
+        array_push($rows, new TableSeparator());
+        $c = count($releases);
+        foreach ($releases as $index => $release) {
+            $date = $this->releaseToDate($release);
+            if ($release === $current) {
+                array_push($rows, ["<fg=yellow>$index</>", "<fg=yellow>$release : {$date->format(self::RELEASE_DATE_FORMAT)}</>"]);
+            } else {
+                array_push($rows, [$index, "$release : {$date->format(self::RELEASE_DATE_FORMAT)}"]);
+            }
+            if ($index < $c) {
+                array_push($rows, new TableSeparator());
+            }
         }
-        $this->process->run("$sudo rm -rf $current", $root);
+        $table->setRows($rows);
+        $table->render();
+    }
 
-        $this->output->line('yellow');
+    /**
+     * Convert the release directory name to a date string
+     */
+    protected function releaseToDate(string $release) : \DateTime
+    {
+        assert(valid_num_args());
 
-        $this->output->header('Rollback Complete');
-        $this->output->line('yellow');
-        $this->output->blank();
+        $parts = explode('-', $release);
+        if (count($parts) === 6) {
+            $dateString = "{$parts[1]}/{$parts[2]}/{$parts[0]} {$parts[3]}:{$parts[4]}:{$parts[5]}";
+            return new \DateTime($dateString);
+        }
+
+        throw new \Exception('Invalid Release Date: ' . $release);
+    }
+
+    /**
+     * Remove the current symlink and create a new one that points to the chosen release
+     */
+    protected function rollbackTo(string $release)
+    {
+        assert(valid_num_args());
+
+        $releasePath = $this->env->getReleasesPath() . $release;
+
+        if (!file_exists($releasePath) || !is_dir($releasePath)) {
+            $this->output->error('The selected release directory (' . $release . ') does not exist');
+        }
+
+        $rootPath = $this->env->getRootPath();
+        $currentPath = $rootPath . 'current';
+
+        $this->output->writeln('Removing old symlink');
+        $this->process->run("rm $currentPath");
+
+        $this->output->writeln('Create new symlink');
+        $this->process->run("ln -s $releasePath $currentPath");
+
+        $this->env->setCurrent($release);
+        $this->env->save();
+
+        $this->output->line();
     }
 }
